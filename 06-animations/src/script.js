80fa8e72931965c6d050e37824f38a2beebecece
ksbisht <ksbisht941@gmail.com>
2022-10-29 00:04:20 +0530

    animations

---
+++ 06-animations/src/script.js
@@ -0,0 +1,55 @@
+import './style.css'
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+
+// let time = Date.now();
+const clock = new THREE.Clock();
+
+const tick = () => {
+    // mesh.position.y += 0.01;
+    // mesh.position.x += 0.01;
+    // mesh.rotation.x += Math.PI * 0.01;
+    
+    // const currentTime = Date.now();
+    // const deltaTime = currentTime - time;
+
+    const elapsedTime = clock.getElapsedTime();
+
+    // mesh.rotation.y = elapsedTime * Math.PI;
+    mesh.position.y = Math.sin(elapsedTime);
+    mesh.position.x = Math.cos(elapsedTime);
+
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(tick)
+}
+
+tick();
